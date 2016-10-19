355d01abb00687eada333dde3741710691801e48
Marica Odagaki <ento.entotto@gmail.com>
2016-10-18 17:01:11 -0700

    Allow controlling s3fs args through an env var

--- scripts/runS3fs.sh
+++ scripts/runS3fs.sh
@@ -1,1 +1,1 @@
-mkdir -p /home/shared/s3 && mkdir -p /tmp && s3fs $BUCKETNAME /home/shared/s3 -o use_cache=/tmp -o allow_other -o umask=0002 -o use_rrs -f
+mkdir -p /home/shared/s3 && mkdir -p /tmp && s3fs $BUCKETNAME $S3FS_ARGS /home/shared/s3 -o use_cache=/tmp -o allow_other -o umask=0002 -o use_rrs -f
