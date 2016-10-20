ffe9f78bebaeb068e1ce3e69ef44d96f8fa50b69
Marica Odagaki <ento.entotto@gmail.com>
2016-10-19 22:41:31 -0700

    Lock down s3fs version

--- scripts/install_s3fs.sh
+++ scripts/install_s3fs.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+git clone -b v1.79 --single-branch https://github.com/s3fs-fuse/s3fs-fuse.git
 cd s3fs-fuse/ && ./autogen.sh && ./configure && make && make install
